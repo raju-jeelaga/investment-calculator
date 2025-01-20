e13278d3a66a0c318672be38f7e4f412268afb35
unknown <rajujeelaga@gmail.com>
2025-01-20 15:29:01 +0530

    uncommit the code, due to bad result
    uncommit the code, due to bad result

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,28 +5,28 @@ export default function Results({input}){
     const resultsData = calculateInvestmentResults(input);
     console.log(resultsData);
     return(
-        <table id="result">
-            <thead>
-                <tr>
-                    <th>Year</th>
-                    <th>Investment Value</th>
-                    <th>Interest (Year)</th>
-                    <th>Total Interest</th>
-                    <th>Invested Capital</th>
-                </tr>
-            </thead>
-            <tbody>
-                {resultsData.map(yearData => (
-                    <tr key={yearData.year}>
-                        <td>{yearData.year}</td>
-                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
-                        <td>{formatter.format(yearData.interest)}</td>
-                        <td>{formatter.format(yearData.totalInterest)}</td>
-                        <td>{formatter.format(yearData.investedCapital)}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        // <table id="result">
+        //     <thead>
+        //         <tr>
+        //             <th>Year</th>
+        //             <th>Investment Value</th>
+        //             <th>Interest (Year)</th>
+        //             <th>Total Interest</th>
+        //             <th>Invested Capital</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         {resultsData.map(yearData => (
+        //             <tr key={yearData.year}>
+        //                 <td>{yearData.year}</td>
+        //                 <td>{formatter.format(yearData.valueEndOfYear)}</td>
+        //                 <td>{formatter.format(yearData.interest)}</td>
+        //                 <td>{formatter.format(yearData.totalInterest)}</td>
+        //                 <td>{formatter.format(yearData.investedCapital)}</td>
+        //             </tr>
+        //         ))}
+        //     </tbody>
+        // </table>
     )
 
 }
